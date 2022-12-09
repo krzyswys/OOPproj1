2621b2695871e680578842655ad3be836ba718db
Jer <jersliw@student.agh.edu.pl>
2022-12-09 15:47:36 +0100

    MapDirection

--- src/main/java/agh/oop/MapDirection.java
+++ src/main/java/agh/oop/MapDirection.java
@@ -18,12 +18,16 @@ public enum MapDirection {
     Vector2d toUnitVector() {
         return switch (this) {
             case NORTH -> new Vector2d(0, 1);
+            case NORTH_EAST -> new Vector2d(1, 1);
             case EAST -> new Vector2d(1, 0);
+            case SOUTH_EAST -> new Vector2d(1, -1);
             case SOUTH -> new Vector2d(0, -1);
+            case SOUTH_WEST -> new Vector2d(-1, -1);
             case WEST -> new Vector2d(-1, 0);
+            case NORTH_WEST -> new Vector2d(-1,1);
         };
     }
-    public static MapDirection fromNubmer(int x) {
+    public static MapDirection fromNumber(int x) {
         for (MapDirection d : MapDirection.values()) {
             if (d.value == x ) {
                 return d;
