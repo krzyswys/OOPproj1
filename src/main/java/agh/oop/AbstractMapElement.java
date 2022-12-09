16b4cf61aa91f6fbb643dcef19c8cd121a338692
Jer <jersliw@student.agh.edu.pl>
2022-12-09 14:59:23 +0100

    MapDirection

--- src/main/java/agh/oop/AbstractMapElement.java
+++ src/main/java/agh/oop/AbstractMapElement.java
@@ -1,14 +1,14 @@
 package agh.oop;
 
 public abstract class AbstractMapElement {
-    protected Vector2d location;
+    protected Vector2d position;
     protected int energy;
 
-    public Vector2d getLocation() {
-        return new Vector2d(location);
+    public Vector2d getPosition() {
+        return new Vector2d(position);
     }
 
     boolean isAt(Vector2d position) {
-        return this.location.equals(position);
+        return this.position.equals(position);
     }
 }
---
+++ src/main/java/agh/oop/MapDirection.java
@@ -0,0 +1,35 @@
+package agh.oop;
+
+public enum MapDirection {
+    NORTH(0),
+    NORTH_EAST(1),
+    EAST(2),
+    SOUTH_EAST(3),
+    SOUTH(4),
+    SOUTH_WEST(5),
+    WEST(6),
+    NORTH_WEST(7);
+
+    final int value;
+
+    MapDirection(int a) {
+        value = a;
+    }
+    Vector2d toUnitVector() {
+        return switch (this) {
+            case NORTH -> new Vector2d(0, 1);
+            case EAST -> new Vector2d(1, 0);
+            case SOUTH -> new Vector2d(0, -1);
+            case WEST -> new Vector2d(-1, 0);
+        };
+    }
+    public static MapDirection fromNubmer(int x) {
+        for (MapDirection d : MapDirection.values()) {
+            if (d.value == x ) {
+                return d;
+            }
+        }
+        throw new IndexOutOfBoundsException(x+" is not a vaild MapDirection");
+    }
+
+}
