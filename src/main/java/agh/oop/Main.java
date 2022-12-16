87c3cf73f59912906b0f50ad25c67fbc78005eef
krzyswys <krzyswys3@gmail.com>
2022-12-17 00:41:37 +0100

    "created simple simulation engine"

--- src/main/java/agh/oop/Main.java
+++ src/main/java/agh/oop/Main.java
@@ -10,26 +10,7 @@ import agh.oop.plant.Trees;
 
 public class Main {
     public static void main(String[] args) {
-        WorldMap map = new WorldMap(new MapSize(15,15), new Earth(), new Toxic());
-        Animal a = new Animal(map, new Vector2d(4, 4));
-        map.createNAnimals(10);
-        System.out.println(a);
-        for (int i = 0; i < 10; ++i) {
-            a.move();
-            System.out.println(a.info());
-        }
-        MapVisualizer mapVisualizer = new MapVisualizer(map);
-        map.createNPlants(90);
-        map.addAnimal(a);
-        System.out.println(mapVisualizer.draw(
-                new Vector2d(0, 0), new Vector2d(map.getSize().getHeight(), map.getSize().getWidth())));
-        a.move();
-        System.out.println(a.info());
-        System.out.println(mapVisualizer.draw(
-                new Vector2d(0, 0), new Vector2d(map.getSize().getHeight(), map.getSize().getWidth())));
-        a.move();
-        System.out.println(a.info());
-        System.out.println(mapVisualizer.draw(
-                new Vector2d(0, 0), new Vector2d(map.getSize().getHeight(), map.getSize().getWidth())));
+        SimulationEngine engine = new SimulationEngine(new MapSize(15,15), new Earth(), new Toxic(), 10, 90);
+        engine.run();
     }
 }
---
+++ src/main/java/agh/oop/SimulationEngine.java
@@ -0,0 +1,50 @@
+package agh.oop;
+
+import agh.oop.animal.Animal;
+import agh.oop.animal.IGeneMutator;
+import agh.oop.animal.INextGene;
+import agh.oop.map.*;
+import agh.oop.plant.IPlantType;
+import agh.oop.plant.Toxic;
+
+import java.util.List;
+
+public class SimulationEngine {
+    MapSize mapSize;
+    IMapType mapType;
+    IPlantType plantType;
+    int animalsToStart;
+    int plantsToStart;
+
+    IGeneMutator geneMutator;
+    INextGene nextGene;
+    WorldMap map;
+    MapVisualizer mapVisualizer;
+
+    public SimulationEngine(MapSize mapSize, IMapType mapType, IPlantType plantType, int animalsToStart, int plantsToStart) {
+        this.mapSize = mapSize;
+        this.mapType = mapType;
+        this.plantType = plantType;
+        this.plantsToStart = plantsToStart;
+        this.animalsToStart = animalsToStart;
+        this.map = new WorldMap(mapSize, mapType, plantType);
+        this.mapVisualizer = new MapVisualizer(map);
+
+    }
+
+    public void run() {
+        map.createNAnimals(animalsToStart);
+        map.createNPlants(plantsToStart);
+        List<Animal> animals = map.getAnimals();
+        for(int i=0; i<10; i++){
+            for (Animal animal : animals) {
+                animal.move();
+            }
+            System.out.println(mapVisualizer.draw(
+                    new Vector2d(0, 0), new Vector2d(map.getSize().getHeight(), map.getSize().getWidth())));
+        }
+
+
+
+    }
+}
